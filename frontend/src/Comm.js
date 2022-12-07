d392e7c2267a409696b461a22a3fdfa8be8808cd
GermanBluefox <dogafox@gmail.com>
2022-12-07 22:56:38 +0100

    Support of dynamic API url

--- frontend/src/Comm.js
+++ frontend/src/Comm.js
@@ -1,4 +1,4 @@
-const URL = window.API_URL && window.API_URL !== '${API_URL}' ? `https://${window.API_URL}/` : 'https://3jjxddo33l.execute-api.eu-west-1.amazonaws.com/default/checkAdapter';
+const URL = window.API_URL && window.API_URL !== '${API_URL}' ? `${window.API_URL}` : 'https://3jjxddo33l.execute-api.eu-west-1.amazonaws.com/default/checkAdapter';
 class Comm {
     static check(repo, branch, cb) {
         const url = `${URL}?url=${encodeURIComponent(repo)}${branch ? `&branch=${encodeURIComponent(branch)}` : ''}`;
